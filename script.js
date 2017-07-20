f1e512fa8b9e67b14a8151f657d97dda5a3a6f27
leoinho <leoinhosagaz@hotmail.com>
2017-07-20 19:38:39 -0300

    adicinando js pagina 1 e 2

--- script.js
+++ script.js
@@ -1,13 +1,119 @@
-function myFunction() {
-    console.log('fui clicado');
-    document.getElementById("urlVideo").src = "video\3030FodaQueElaÉLinda(Acústico).mp4";
-  novaurl = document.getElementById("urlVideo");
-  console.log("fui clicado");
+
+function videocentral1() {
+   
+    document.getElementById("video1").src = "video/video1.mp4";
 }
-function minhaFuncao() {
-    elemento = document.getElementById('p').innerHTML = "aeeee porraaaa";
-    }
- function videocentral() {
-   document.getElementById('aqui');
-    console.log('aeee');
+function videocentral2() {
+   
+    document.getElementById("video1").src = "video/video2.mp4";
+}
+function videocentral3() {
+ 
+    document.getElementById("video1").src = "";
+}
+function videocentral4() {
+  
+    document.getElementById("video1").src = "";
+}
+function videocentral5() {
+ 
+    document.getElementById("video1").src = "";
+}
+function videocentral11() {
+ 
+    document.getElementById("video2").src = "";
+}
+function videocentral12() {
+  
+    document.getElementById("video2").src = "";
+}
+function videocentral13() {
+  
+    document.getElementById("video2").src = "";
+}
+function videocentral14() {
+
+    document.getElementById("video2").src = "";
+}
+function videocentral15() {
+
+    document.getElementById("video2").src = "";
+}function videocentral21() {
+
+    document.getElementById("video3").src = "";
+}
+function videocentral22() {
+ 
+    document.getElementById("video3").src = "";
+}
+function videocentral23() {
+
+    document.getElementById("video3").src = "";
+}
+function videocentral24() {
+
+    document.getElementById("video3").src = "";
+}
+function videocentral25() {
+
+    document.getElementById("video3").src = "";
+}
+function videocentral31() {
+   
+    document.getElementById("video4").src = "video/video1.mp4";
+}
+function videocentral32() {
+   
+    document.getElementById("video4").src = "video/video2.mp4";
+}
+function videocentral33() {
+
+    document.getElementById("video4").src = "";
+}
+function videocentral34() {
+ 
+    document.getElementById("video4").src = "";
+}
+function videocentral35() {
+
+    document.getElementById("video4").src = "";
+}
+function videocentral41() {
+
+    document.getElementById("video5").src = "";
+}
+function videocentral42() {
+  
+    document.getElementById("video5").src = "";
+}
+function videocentral43() {
+ 
+    document.getElementById("video5").src = "";
+}
+function videocentral44() {
+  
+    document.getElementById("video5").src = "";
+}
+function videocentral45() {
+  
+    document.getElementById("video5").src = "";
+}function videocentral51() {
+  
+    document.getElementById("video6").src = "";
+}
+function videocentral52() {
+
+    document.getElementById("video6").src = "";
+}
+function videocentral53() {
+  
+    document.getElementById("video6").src = "";
+}
+function videocentral54() {
+   
+    document.getElementById("video6").src = "";
+}
+function videocentral55() {
+
+    document.getElementById("video6").src = "";
 }
