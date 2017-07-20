5c75fe94c7a3f1caa9d498ecb8f49590ba91ffb8
leoinho <leoinhosagaz@hotmail.com>
2017-07-19 22:30:50 -0300

    adicionando js e carousel

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    console.log('fui clicado');
+    document.getElementById("urlVideo").src = "video\3030FodaQueElaÉLinda(Acústico).mp4";
+  novaurl = document.getElementById("urlVideo");
+  console.log("fui clicado");
+}
+function minhaFuncao() {
+    elemento = document.getElementById('p').innerHTML = "aeeee porraaaa";
+    }
+ function videocentral() {
+   document.getElementById('aqui');
+    console.log('aeee');
+}
